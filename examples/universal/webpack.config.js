5d2f690ffd096217117c0a3d681cee0a4aee7e1b
kulinich denis <kulinichdenis@kulinichs-MacBook-Pro.local>
2016-09-08 16:03:02 +0300

    add web pack

--- examples/universal/webpack.config.js
+++ examples/universal/webpack.config.js
@@ -26,6 +26,31 @@ module.exports = {
         query: {
           presets: [ 'react-hmre' ]
         }
+      },
+      { test: /\.css$/, loader: "style-loader!css-loader" },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url?limit=10000&mimetype=application/octet-stream'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'file'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url?limit=10000&mimetype=image/svg+xml'
+      },
+      { 
+        test: /\.png$/, 
+        loader: "url-loader?limit=100000" 
+      },
+      { 
+        test: /\.jpg$/, 
+        loader: "file-loader" 
       }
     ]
   }
